90635beb90f9ef9ad584306a1b4623c39ede4d85
araanv <ameliaarahayu138@gmail.com>
2023-11-08 14:20:09 +0700

    praktikum 8 dan 9

--- MobileNavLanjutan/app/src/main/java/com/example/mobilenavlanjutan/MainActivity.java
+++ MobileNavLanjutan/app/src/main/java/com/example/mobilenavlanjutan/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 @Override
                 public boolean onQueryTextChange(String newText) {
+
                     return false;
                 }
             });
